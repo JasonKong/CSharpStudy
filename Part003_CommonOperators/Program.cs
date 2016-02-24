e8ef7142bcd6a010a485eebf326de93ee6213246
Jason Kong <jasonkongnz@gmail.com>
2016-02-24 23:36:08 +1300

    Common operator and ternary operator

---
+++ Part003_CommonOperators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/*
+    Assignment Operator =
+    Arithmetic Operator +, -, *, /, %
+    Comparison Operator ==, !=, >, >=, <, <=
+    Conditional Operator &&, ||
+    Ternary Operator ?:
+    Null Coalescing ?? 空接合运算符
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
+        bool isPassed = false;
+
+        // without ternary operator
+        //if (number1 >= 60 && number2 >= 60)
+        //{
+        //    isPassed = true;
+        //}
+
+        // using ternary operator
+        isPassed = (number1 >= 60 && number2 >= 60) ? true : false;
+        Console.WriteLine("Number: {0} and {1} is {2}", number1, number2, isPassed);
+    }
+}
+
