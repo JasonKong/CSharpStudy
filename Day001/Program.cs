1650e2c1cf9b1f2a20144a5c2f8fde37f261f86e
Jason Kong <jasonkongnz@gmail.com>
2016-02-24 23:07:14 +1300

    Writing to console and reading from console. Using place holder syntax to write to console

---
+++ Day001/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // writing to console
+        Console.WriteLine("Please enter your first name:");
+        // reading from console
+        string FirstName = Console.ReadLine();
+
+        Console.WriteLine("Please enter your last name:");
+        string LastName = Console.ReadLine();
+
+        // 2 ways to write to console
+        // Concactenation
+        // Console.WriteLine("Hello " + FirstName + " " + LastName);
+        // Place holder syntax -- Most preferred
+        Console.WriteLine("Hello {0} {1}", FirstName, LastName);
+    }
+}
