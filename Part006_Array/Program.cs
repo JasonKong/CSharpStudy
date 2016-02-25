55d5186cd611feab9ef8acd44171473bcc1851d1
Jason Kong <jasonkongnz@gmail.com>
2016-02-26 00:21:42 +1300

    Array

---
+++ Part006_Array/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+    An array is a collection of similar data types.
+        Advantages: arrays are strong typed.
+        Disadvantages: 
+         1 The size of array cannot be increased once initialized.
+         2 Have to rely on integral indices to store or retrieve items from the array.
+*/
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] ages = new int[3];
+
+        ages[0] = 32; // index start from 0
+        ages[1] = 28;
+        ages[2] = 3;
+        //ages[2] = "age"; // Compile error because of different type
+        //ages[3] = 10; // Runtime error: IndexOutOfRange exception
+
+        Console.WriteLine(ages[1]);
+    }
+}
+
