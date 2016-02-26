58aa1b25b2b2d4927a3532c7599b932a23d92488
Jason Kong <jasonkongnz@gmail.com>
2016-02-26 23:17:10 +1300

    comment in C#

---
+++ Part007_Comments/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+/// <summary>
+/// Three ways of comment in C#
+/// Single line comments   //
+/// Multi line comments    /*   */   
+/// XML Document comments (just like this block of code using ///)
+/// Keyboard shortcut: 
+///     comment: Ctrl + K, Ctrl + C
+///     uncomment: Ctrl + K, Ctrl + U
+/// </summary>
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Captial of China        Single line comments
+        string capital = "Beijing";
+        string country = "";
+
+        /*
+            if capital is Beijing, 
+            country will be assigned as China   Multi line comments
+        */
+        if (capital == "Beijing")
+        {
+            country = "China";
+        }
+    }
+}
+
+
